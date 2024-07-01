a4bafdfde15dd278af78f9a3fd14ee9b124535ae
fearlessfe <505380967@qq.com>
2024-07-01 22:26:51 +0800

    feat: add copy to game address

--- src/pages/Dashboard/CreditList.tsx
+++ src/pages/Dashboard/CreditList.tsx
@@ -1,13 +1,17 @@
+import ContentCopyIcon from '@mui/icons-material/ContentCopy';
 import {
   Avatar,
   List,
   ListItem,
   ListItemAvatar,
   ListItemText,
+  Snackbar,
+  Stack,
   Typography,
 } from '@mui/material';
 import { ethers } from 'ethers';
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
+import CopyToClipboard from 'react-copy-to-clipboard';
 import { useNavigate } from 'react-router-dom';
 import bonus from '../../assets/images/bonus.png';
 import { Credit } from '../../lib/types';
@@ -18,31 +22,53 @@ interface CreditListProps {
 }
 const CreditList: FC<CreditListProps> = ({ credits }) => {
   const nav = useNavigate();
+  const [open, setOpen] = useState<boolean>(false);
   return (
-    <List>
-      {credits?.map((item, index) => (
-        <ListItem alignItems="flex-start" key={item.address}>
-          <ListItemAvatar>
-            <Avatar alt="game" src={bonus} />
-          </ListItemAvatar>
-          <ListItemText
-            primary={ethers.formatEther(item.amount).substring(0, 5)}
-            secondary="ETH"
-          ></ListItemText>
-          <ListItemText
-            primary="Address"
-            secondary={
-              <React.Fragment>
-                <Typography onClick={() => nav(`/credits/${item.address}`)}>
-                  {formatAddress(item.address)}
-                </Typography>
-              </React.Fragment>
-            }
-          ></ListItemText>
-          <ListItemAvatar>{index + 1}</ListItemAvatar>
-        </ListItem>
-      ))}
-    </List>
+    <>
+      <Snackbar
+        open={open}
+        autoHideDuration={2000}
+        message="Copied"
+        anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+        onClose={() => setOpen(false)}
+      />
+      <List>
+        {credits?.map((item, index) => (
+          <ListItem alignItems="flex-start" key={item.address}>
+            <ListItemAvatar>
+              <Avatar alt="game" src={bonus} />
+            </ListItemAvatar>
+            <ListItemText
+              primary={ethers.formatEther(item.amount).substring(0, 5)}
+              secondary="ETH"
+            ></ListItemText>
+            <ListItemText
+              primary="Address"
+              secondary={
+                <React.Fragment>
+                  <Stack direction={'row'} gap={2}>
+                    <Typography
+                      component={'span'}
+                      onClick={() => nav(`/games/${item.address}`)}
+                      sx={{ color: 'primary.main', cursor: 'pointer' }}
+                    >
+                      {formatAddress(item.address)}
+                    </Typography>
+                    <CopyToClipboard
+                      text={item.address}
+                      onCopy={(text, res) => setOpen(res)}
+                    >
+                      <ContentCopyIcon sx={{ color: 'black' }} />
+                    </CopyToClipboard>
+                  </Stack>
+                </React.Fragment>
+              }
+            ></ListItemText>
+            <ListItemAvatar>{index + 1}</ListItemAvatar>
+          </ListItem>
+        ))}
+      </List>
+    </>
   );
 };
 
--- src/pages/Dashboard/GameList.tsx
+++ src/pages/Dashboard/GameList.tsx
@@ -1,13 +1,17 @@
+import ContentCopyIcon from '@mui/icons-material/ContentCopy';
 import {
   Avatar,
   CircularProgress,
   ListItem,
   ListItemAvatar,
   ListItemText,
+  Snackbar,
+  Stack,
   Typography,
 } from '@mui/material';
 import { List } from 'antd';
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
 import { useNavigate } from 'react-router-dom';
 import gameImg from '../../assets/images/game.png';
 import shield from '../../assets/images/shield.png';
@@ -21,50 +25,69 @@ interface GameListProps {
 
 const GameList: FC<GameListProps> = ({ games }) => {
   const nav = useNavigate();
+  const [open, setOpen] = useState<boolean>(false);
   return (
-    <List>
-      {games?.map((game) => (
-        <ListItem alignItems="flex-start" key={game.id}>
-          <ListItemAvatar>
-            <Avatar alt="game" src={gameImg} />
-          </ListItemAvatar>
-          <ListItemText
-            primary={game.l2_block_number}
-            secondary={
-              <React.Fragment>
-                <Typography component={'span'}>
-                  {calculateDate(new Date(game.block_time * 1000))}
-                </Typography>
-              </React.Fragment>
-            }
-          ></ListItemText>
-          <ListItemText
-            primary="Game Address"
-            secondary={
-              <React.Fragment>
-                <Typography
-                  component={'span'}
-                  onClick={() => nav(`/games/${game.game_contract}`)}
-                >
-                  {formatAddress(game.game_contract)}
-                </Typography>
-              </React.Fragment>
-            }
-          ></ListItemText>
-          <ListItemAvatar>
-            {game.status === 0 && (
-              <CircularProgress color="primary" disableShrink />
-            )}
-            {game.status === 1 && (
-              <img src={sword} width={48} height={48} alt="sword" />
-            )}
-            {game.status === 2 && (
-              <img src={shield} width={48} height={48} alt="shield" />
-            )}
-          </ListItemAvatar>
-        </ListItem>
-      ))}
-    </List>
+    <>
+      <Snackbar
+        open={open}
+        autoHideDuration={2000}
+        message="Copied"
+        anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+        onClose={() => setOpen(false)}
+      />
+      <List>
+        {games?.map((game) => (
+          <ListItem alignItems="flex-start" key={game.id}>
+            <ListItemAvatar>
+              <Avatar alt="game" src={gameImg} />
+            </ListItemAvatar>
+            <ListItemText
+              primary={game.l2_block_number}
+              secondary={
+                <React.Fragment>
+                  <Typography component={'span'}>
+                    {calculateDate(new Date(game.block_time * 1000))}
+                  </Typography>
+                </React.Fragment>
+              }
+            ></ListItemText>
+            <ListItemText
+              primary="Game Address"
+              secondary={
+                <React.Fragment>
+                  <Stack direction={'row'} gap={2}>
+                    <Typography
+                      component={'span'}
+                      onClick={() => nav(`/games/${game.game_contract}`)}
+                      sx={{ color: 'primary.main', cursor: 'pointer' }}
+                    >
+                      {formatAddress(game.game_contract)}
+                    </Typography>
+                    <CopyToClipboard
+                      text={game.game_contract}
+                      onCopy={(text, res) => setOpen(res)}
+                    >
+                      <ContentCopyIcon sx={{ color: 'black' }} />
+                    </CopyToClipboard>
+                  </Stack>
+                </React.Fragment>
+              }
+            ></ListItemText>
+            <ListItemAvatar>
+              {game.status === 0 && (
+                <CircularProgress color="primary" disableShrink />
+              )}
+              {game.status === 1 && (
+                <img src={sword} width={48} height={48} alt="sword" />
+              )}
+              {game.status === 2 && (
+                <img src={shield} width={48} height={48} alt="shield" />
+              )}
+            </ListItemAvatar>
+          </ListItem>
+        ))}
+      </List>
+    </>
   );
 };
 
