3d2066c3314a1710e746fd08a3847d4905c2c926
fearlessfe <505380967@qq.com>
2024-07-14 18:51:48 +0800

    feat: 增加url跳转

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,13 +1,19 @@
 import GitHubIcon from '@mui/icons-material/GitHub';
 import XIcon from '@mui/icons-material/X';
+import { Link } from '@mui/material';
 
 const Footer = () => {
   return (
     <div className="py-6">
       <div className="flex justify-center">
-        <div className="p-3">
-          <GitHubIcon />
-        </div>
+        <Link sx={{ padding: '12px' }}>
+          <a
+            target="_blank"
+            href="https://github.com/optimism-java/dispute-explorer-frontend"
+          >
+            <GitHubIcon />
+          </a>
+        </Link>
         <div className="p-3">
           <XIcon />
         </div>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,44 @@
+import { mdiSourceFork, mdiSourceRepository, mdiStarOutline } from '@mdi/js';
+import Icon from '@mdi/react';
+import { Link } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
 
 const Header = () => {
   const nav = useNavigate();
   return (
-    <div className="flex h-16 items-center bg-slate-50 px-6">
+    <div className="flex h-16 items-center justify-between bg-slate-50 px-6">
       <div className="font-dogica" onClick={() => nav('/')}>
         Dispute Game
       </div>
+      <Link underline="none">
+        <a
+          target="_blank"
+          href="https://github.com/optimism-java/dispute-explorer-frontend"
+        >
+          <div className="flex pr-8 text-slate-500">
+            <div>
+              <Icon path={mdiSourceRepository} size={2} />
+            </div>
+            <div className="flex flex-col font-mono text-sm">
+              <div>dispute-explorer-frontend</div>
+              <div className="flex items-center justify-around">
+                <div className="flex items-center">
+                  <span>
+                    <Icon path={mdiStarOutline} size={1} />
+                  </span>
+                  <span className="px-4">0</span>
+                </div>
+                <div className="flex items-center">
+                  <span>
+                    <Icon path={mdiSourceFork} size={1} />
+                  </span>
+                  <span className="px-8">0</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </a>
+      </Link>
     </div>
   );
 };
