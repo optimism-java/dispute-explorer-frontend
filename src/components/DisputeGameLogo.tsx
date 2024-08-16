0cc6c32da27e893a434ba106cdf31617fd23a93c
lance10030 <lance10030@163.com>
2024-08-16 11:14:11 +0800

    feat: logo change

--- src/components/DisputeGameLogo.tsx
+++ src/components/DisputeGameLogo.tsx
@@ -1,11 +1,9 @@
-import { useTheme } from "next-themes";
 import Link from "next/link";
 
 
 const DisputeGameLogo: React.FC<{ className?: string }> = ({ }) => {
-  const { resolvedTheme } = useTheme()
   return <Link href="/">
-    <img src={resolvedTheme === 'light' ? '/logo_light.svg' : '/logo.svg'} className="w-48" alt="logo" />
+    <img src='/logo.svg' className="w-48" alt="logo" />
   </Link>
 };
 
--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -2,10 +2,9 @@ import { useTheme } from "next-themes";
 import Link from "next/link";
 
 export const Logo: React.FC<{ className?: string }> = ({ className = "" }) => {
-  const { resolvedTheme } = useTheme()
   return (
     <Link href="/">
-      <img src={resolvedTheme === 'light' ? '/logo_light.svg' : '/logo.svg'} className="w-48" alt="logo" />
+      <img src='/logo.svg' className="w-40" alt="logo" />
     </Link>
   );
 }
