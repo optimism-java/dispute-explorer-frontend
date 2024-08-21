20579f2dd98053690188c97b3c361da23b36673b
lance10030 <lance10030@163.com>
2024-08-21 17:40:04 +0800

    feat: text change

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -3,7 +3,7 @@ export type Network = 'mainnet' | 'sepolia' | 'base-sepolia'
 export const EXPLORER_L1 = process.env.NEXT_PUBLIC_L1_EXPLORER || 'https://sepolia.etherscan.io'
 export const EXPLORER_L2 = process.env.NEXT_PUBLIC_L2_EXPLORER || 'https://sepolia-optimism.etherscan.io'
 export const network = process.env.NEXT_PUBLIC_NETWORK as Network || 'sepolia'
-export const ApiDoc = process.env.NEXT_API_DOC || ''
+export const ApiDoc = process.env.NEXT_PUBLIC_API_DOC || ''
 
 export const SuperProofUrls: Record<Network, string> = {
   'mainnet': process.env.NEXT_PUBLIC_OP_MAINNET_URL || '',
