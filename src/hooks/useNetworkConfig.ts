ab7e142960a8a7069a99792804b5d3e9e6af580a
fearlessfe <505380967@qq.com>
2024-08-25 15:23:16 +0800

    fix: link error

--- src/hooks/useNetworkConfig.ts
+++ src/hooks/useNetworkConfig.ts
@@ -2,13 +2,13 @@ import { NetworkConfig, networkConfigs, Network } from "@/utils/env";
 import { useMemo } from "react";
 
 export const useNetworkConfig = (): NetworkConfig => {
-  const origin = window.location.origin;
+  const host = window.location.host;
   const defaultNetwork = networkConfigs["sepolia"];
   defaultNetwork.network = "sepolia";
   const config = useMemo(() => {
     for (const key of Object.keys(networkConfigs)) {
       const val = networkConfigs[key as Network];
-      if (val && val.origin === origin) {
+      if (val && val.origin === host) {
         val.network = key as Network;
         return val;
       }
