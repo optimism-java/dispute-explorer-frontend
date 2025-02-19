ef0c6899c188b9c0fc6e49777968c45d2e4031a7
fearlseefe <505380967@qq.com>
2025-02-19 11:31:03 +0800

    fix: base logo corlor

--- src/components/DisputeGameLogo.tsx
+++ src/components/DisputeGameLogo.tsx
@@ -4,10 +4,11 @@ import { useContext } from "react";
 
 const DisputeGameLogo: React.FC<{ className?: string }> = ({}) => {
   const { network } = useContext(NetworkConfigContext);
+  const isBase = network.indexOf("base") !== -1;
   return (
     <Link href="/">
       <img
-        src={network === "base-sepolia" ? "/logo_base.svg" : "/logo.svg"}
+        src={isBase ? "/logo_base.svg" : "/logo.svg"}
         className="w-48"
         alt="logo"
       />
